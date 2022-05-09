31c991a963515d85b68e90c2e216ed33e760fbda
Enzo Benito <benitoen@ens-epua.univ-savoie.fr>
2022-05-09 16:33:30 +0200

    fez

--- lespages/technologie.php
+++ lespages/technologie.php
@@ -33,52 +33,105 @@ include 'php_functions/bdd_connect.php'
 	   <div id=main>
 
 	   		<div id="titre">
-	   			<h1 style="text-align:center;">Technologies</h1>
+	   			<h1 style="text-align:center">Technologies</h1>
 	   		</div>
 
-	   </div>
+	   
 
-</body>
 
-=======
-﻿<html> 
 
-   <head>
-   
-	    <meta charset="UTF-8"> 
-		<link rel="stylesheet" href="home.css">
+	   		<div id="capteur">
+	   			<h1 style="text-align:center">Capteurs utilisés</h1>
+			   	<div id="c1">
+				   
+				   <?php 
+				   $sql= "SELECT DISTINCT (nom ,types, position, unite, descript) FROM AppVOLET_Techno  WHERE  types = 'Capteur' and  nom = 'Capteur de temperature'";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li>\n";
+				   echo "\n";
+				   echo "<li>"."Les capteurs de températures sont positionés".$row["position"]."</li>\n";
+				   echo"<li>".$row["descript"]."</li>\n";
+				   
+				   ?>
+			   </div>
+
+			   <div id="c2">
+			   <?php 
+				   $sql= "SELECT DISTINCT (nom ,types, position, unite, descript) FROM AppVOLET_Techno  WHERE  types = capteur and  nom= Capteur de luminosite";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li>\n";
+				   echo "\n";
+				   echo "<li>"."Les capteurs de Luminosités sont positionés".$row["position"]."</li>\n";
+				   echo"<li>".$row["descript"]."</li>\n";
+				   
+				   ?>
+			   
+			   </div>
+
+			   <div id="c3">
+			   <?php 
+				   $sql= "SELECT DISTINCT (nom ,types, position, unite, descript) FROM AppVOLET_Techno  WHERE  types = capteur and  nom= position";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li>\n";
+				   echo "\n";
+				   echo "<li>"."Les capteurs de Positions sont positionés".$row["position"]."</li>\n";
+				   echo"<li>".$row["descript"]."</li>\n";
+				   
+				   ?>
+
+			   </div>
+
+			   <div id="c4">
+			   <?php 
+				   $sql= "SELECT DISTINCT (nom ,types, position, unite, descript) FROM AppVOLET_Techno  WHERE  types = capteur and  nom= courant";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li>\n";
+				   echo "\n";
+				   echo "<li>"."Les capteurs de courants sont positionés".$row["position"]."</li>\n";
+				   echo"<li>".$row["descript"]."</li>\n";
+				   
+				   ?>
+			   </div>
+			</div>
+
+			<div id="actionneur">
+	   			<h1 style="text-align:center">Actionneurs utilisés</h1>
+			    <div id="a1">
+				   
+			   
+			   </div>
+
+			   <div id="a2">
+			   </div>
+
+			  
+
+			</div>
 		
-   </head> 
+			<div id="inteligence">
+	   			<h1 style="text-align:center">Inteligence</h1>
+			   <div id="i1">
+			   </div>
 
-   <body> 
-	   <div id=menu>
-
-		   <div id="espace">
-
-		   </div>
-
-	   		<a href="home.html" class="bouttons_menu" id="boutton_menu">Menu</a>
-	   		<a href="technologie.html" class="bouttons_menu" id="boutton_technologie">Technologies</a>
-	   		<a href="groupe.html" class="bouttons_menu" id="boutton_groupe">Groupe</a>
-	   		<a href="quizz.html" class="bouttons_menu" id="boutton_quizz">Quizz</a>
-	   		<a href="mesures.html" class="bouttons_menu" id="boutton_mesures">Mesures</a>
-
-	   		<div id="pied">
-	   			<span>Polytech Annecy-Chambéry promotion 2024</span>
-	   		</div>
-
-	   </div>
-		
-	   <div id=main>
-
-	   		<div id="titre">
-	   			<h1 style="text-align:center;">Technologies</h1>
-	   		</div>
-
-	   </div>
-
-</body>
+			   
+		</div>
 
 
-
+	</body>
 </html>
