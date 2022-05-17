e5039003de41b852a9128cd50b214410b3ca784a
Nathan Dalenc <Nathan.Dalenc@etu.univ-savoie.fr>
2022-05-17 10:06:13 +0200

    Amen

--- lespages/technologie.php
+++ lespages/technologie.php
@@ -10,7 +10,8 @@ include 'php_functions/bdd_connect.php'
    
 	    <meta charset="UTF-8"> 
 		<link rel="stylesheet" href="css/main.css">
-		
+		<link rel="stylesheet" href="css/technologie.css">
+
    </head> 
 
    <body> 
@@ -33,70 +34,70 @@ include 'php_functions/bdd_connect.php'
 			   	<div id="c1">
 				   
 				   <?php 
-				   $sql= "SELECT DISTINCT nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Capteur' and  nom = 'Capteur de temperature'";
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Capteur' and  nom = 'Capteur de temperature'";
 
 				   $result = BDD_request($sql);
 
-				   $row = mysqli_fetch_array($result);
+				   $row = mysqli_fetch_assoc($result);
 
-				   echo"<li>".$row["nom"]."</li><br>";
-				   echo "<br>";
-				   echo "<li>"."Les capteurs de températures sont positionés".$row["position"]."</li><br>";
-				   echo"<li>".$row["descript"]."</li><br>";
+				   echo"<li id =c11>".$row["nom"]."</li><br>";
 				   
+				   echo "<li id = c12>"."Les capteurs de températures sont positionés à l' ".$row["position"]."</li><br>";
+				   echo"<li id = c13>".$row["descript"]."</li><br>";
+				   echo "<br>";
 				   ?>
 			   </div>
 
 			   <div id="c2">
 			   <?php 
-			   /*
-				   $sql= "SELECT DISTINCT nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = capteur and  nom= Capteur de luminosite";
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Capteur 'and  nom= 'Capteur de luminosite' ";
 
 				   $result = BDD_request($sql);
 
-				   $row = mysqli_fetch_array($result);
+				   $row = mysqli_fetch_assoc($result);
 
-				   echo"<li>".$row["nom"]."</li><br>";
+				   echo"<li id = c21>".$row["nom"]."</li><br>";
+				   
+				   echo "<li id = c22>"."Les capteurs de Luminosités sont positionés".$row["position"]."</li><br>";
+				   echo"<li id = c23>".$row["descript"]."</li><br>";
 				   echo "<br>";
-				   echo "<li>"."Les capteurs de Luminosités sont positionés".$row["position"]."</li><br>";
-				   echo"<li>".$row["descript"]."</li><br>";
-				   */
 				   ?>
 			   
 			   </div>
 
 			   <div id="c3">
 			   <?php 
-			   /*
-				   $sql= "SELECT DISTINCT nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = capteur and  nom= position";
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Capteur' and  nom= 'Capteur de presence'";
 
 				   $result = BDD_request($sql);
 
 				   $row = mysqli_fetch_array($result);
 
-				   echo"<li>".$row["nom"]."</li><br>";
+				   echo"<li id = c31>".$row["nom"]."</li><br>";
+				   
+				   echo "<li id = c32>"."Les capteurs de Présence sont positionés".$row["position"]."</li><br>";
+				   echo"<li id = c33>".$row["descript"]."</li>\<br>";
 				   echo "<br>";
-				   echo "<li>"."Les capteurs de Positions sont positionés".$row["position"]."</li><br>";
-				   echo"<li>".$row["descript"]."</li>\<br>";
-				  */ 
 				   ?>
 
 			   </div>
 
 			   <div id="c4">
 			   <?php 
-			   /*
-				   $sql= "SELECT DISTINCT nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = capteur and  nom= courant";
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Capteur' and  nom= 'Capteur de courant'";
 
 				   $result = BDD_request($sql);
 
 				   $row = mysqli_fetch_array($result);
 
-				   echo"<li>".$row["nom"]."</li><br>";
+				   echo"<li id = c41>".$row["nom"]."</li><br>";
+				   
+				   echo "<li id = c42>"."Les capteurs de courants sont positionés".$row["position"]."</li><br>";
+				   echo"<li id = c43>".$row["descript"]."</li><br>";
 				   echo "<br>";
-				   echo "<li>"."Les capteurs de courants sont positionés".$row["position"]."</li><br>";
-				   echo"<li>".$row["descript"]."</li><br>";
-				   */
 				   ?>
 			   </div>
 			</div>
@@ -104,11 +105,38 @@ include 'php_functions/bdd_connect.php'
 			<div id="actionneur">
 	   			<h1 style="text-align:center">Actionneurs utilisés</h1>
 			    <div id="a1">
-				   
+				   <?php 
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Actionneur' and  nom= 'Moteur'";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li><br>";
+				 
+				   echo "<li>"."Les ".$row["nom"]." sont positionés au niveau des".$row["position"]."</li><br>";
+				   echo"<li>".$row["descript"]."</li><br>";
+				   echo "<br>";
+				   ?>
 			   
 			   </div>
 
 			   <div id="a2">
+			   <?php 
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Actionneur' and  nom= 'Pont en H'";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li><br>";
+				   echo "<li>"."Les ".$row["nom"]." sont positionés au niveau du".$row["position"]."</li><br>";
+				   echo"<li>".$row["descript"]."</li><br>";
+				   echo "<br>";
+				   ?>
+
 			   </div>
 
 			  
@@ -118,6 +146,19 @@ include 'php_functions/bdd_connect.php'
 			<div id="inteligence">
 	   			<h1 style="text-align:center">Inteligence</h1>
 			   <div id="i1">
+			   <?php 
+			   
+				   $sql= "SELECT  nom ,types, position, unite, descript FROM AppVOLET_Techno  WHERE  types = 'Intelligence' and  nom= 'Raspberry'";
+
+				   $result = BDD_request($sql);
+
+				   $row = mysqli_fetch_array($result);
+
+				   echo"<li>".$row["nom"]."</li><br>";
+				   echo "<li>"."La ".$row["nom"]." est positionés au niveau du".$row["position"]."</li><br>";
+				   echo"<li>".$row["descript"]."</li><br>";
+				   echo "<br>";
+				   ?>
 			   </div>
 
 			   
