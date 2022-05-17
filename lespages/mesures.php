9df4a10a40fccbf0e3a18c3d5823240e77c8f74c
Johan Meyer <johanmeyer@vpn-253108.local.univ-savoie.fr>
2022-05-17 11:08:44 +0200

    jdjd

---
+++ lespages/mesures.php
@@ -0,0 +1,60 @@
+﻿<?php
+session_save_path("../sessionPhp");
+session_start();
+include 'php_functions/bdd_connect.php'
+?>
+
+<html> 
+
+   <head>
+   
+	    <meta charset="UTF-8">
+		<link rel="stylesheet" href="css/mesures.css">
+		<link rel="stylesheet" href="css/main.css">
+		
+   </head> 
+
+   <body>
+		<?php
+			include 'php_functions/menu.php'
+		?>
+
+
+		<div id=main>
+
+	   		<div id="titre">
+	   			<h1 style="text-align:center">Mesures</h1>
+	   		</div>
+			
+			<?php
+			$sql = "SELECT * FROM AppVOLET_Mesures";
+
+			$result = BDD_request($sql);
+
+			while ($row = mysqli_fetch_assoc($result)) {  
+				echo "<p>\n";
+				echo "<div class=\"mesure\">\n";
+				echo "<p>\n";
+				echo "<span>"."id mesure: ". $row["id_mesure"]. "</span>\n";
+				echo "<br>";
+				echo "<span>"."id technologie: ".$row["id_techno"]. "</span>\n";
+				echo "<br>";
+				echo "<span>"."date: ".$row["instant"]. "</span>\n";
+				echo "<br>";
+				echo "<span>"."valeur de la mesure: ".$row["valeur"]. "</span>\n";
+				echo "</div>\n";
+			}
+			echo "</div>\n";
+
+
+			?>
+		</div>
+
+	   	﻿
+	   		
+		   
+		
+
+
+	</body>
+</html>
