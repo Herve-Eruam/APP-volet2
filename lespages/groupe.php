0cac79d52e3b50609d2ee6dc4950acb38e2a6acb
Herve-Eruam <94961953+Herve-Eruam@users.noreply.github.com>
2022-05-19 18:32:57 +0200

    mise en place options d'ajout/suppr membre

--- lespages/groupe.php
+++ lespages/groupe.php
@@ -2,11 +2,38 @@
 session_save_path("../sessionPhp");
 session_start();
 include 'php_functions/bdd_connect.php';
-if(!empty($_GET['membre_select']) and !empty($_GET['submit'])){
+if(!empty($_GET['membre_select']) and !empty($_GET['submitDesc'])){
 	$sql_request = "UPDATE AppVOLET_Groupe SET descriptions = \"" .$_GET['description'] ."\" WHERE id_membre = " .$_GET["membre_select"] ;
 	$result = BDD_request($sql_request);
 
 	}
+$resAddMember = 0;//rien
+if(!empty($_GET['btn_ajout_membre'])){
+	if($_GET['mdp'] == $_GET['mdp_conf']){
+		$sql_request = "INSERT INTO AppVOLET_Groupe (nom, prenom, descriptions, pass) VALUES ('" .$_GET['nom'] ."', '" .$_GET['prenom']  ."', '" .$_GET['description']  ."', '" .md5($_GET['mdp'])  ."')";
+		$res = BDD_request($sql_request);
+		if($res != false){
+			$resAddMember = 1; //membre ajouté
+			$sql_request = "SELECT id_membre FROM AppVOLET_Groupe WHERE nom = \"" .$_GET['nom'] ."\" and prenom = \"" .$_GET['prenom'] ."\"";$result = BDD_request("SELECT nom, prenom, id_membre FROM AppVOLET_Groupe");
+			$result = BDD_request($sql_request);
+			$val = mysqli_fetch_array($result);	
+			$_GET['membre_select'] = $val["id_membre"];
+		}else{
+			$resAddMember = 3; // erreur inatendu
+
+		}
+	}else{
+		$resAddMember = 2; // mdp ne correspondent pas
+	}
+							
+	}
+	
+	if(!empty($_GET['deleteMember']) and $_GET['deleteMember'] and !empty($_GET['membre_selectForSuppr']) ){
+	$sql_request = "DELETE FROM `AppVOLET_Groupe` WHERE `id_membre` = " .$_GET['membre_selectForSuppr'] ;
+	$result = BDD_request($sql_request);
+	
+	}
+
 ?>
 <html> 
 
@@ -68,8 +95,12 @@ if(!empty($_GET['membre_select']) and !empty($_GET['submit'])){
 									<textarea name="description" rows="8" cols="45" maxlength=200>Nouvelle description...</textarea>
 									<input type="hidden" name="membre_select" value=<?php echo($_GET["membre_select"]); ?> />
 
-									<p><button type="submit" name="submit" value = "true">modifier</button></p>
+									<p><button type="submit" name="submitDesc" value = "true">modifier</button></p>
 								</form>
+									<form action="groupe.php" method="get">
+										<input type="hidden" name="membre_selectForSuppr" value=<?php echo($_GET["membre_select"]); ?> />
+										<p><button type="submit" name="deleteMember" value = "true">supprimer le membre (irreversible)</button></p>
+									</form>
 							</div>
 					<?php
 							}
@@ -83,14 +114,16 @@ if(!empty($_GET['membre_select']) and !empty($_GET['submit'])){
 			?>
 			<div id=AddMembreAll>
 				<div id=AddMemberTitre>
-					  &nbsp &nbsp &nbsp &nbsp Ajouter un membre au groupe 
+					  &nbsp &nbsp &nbsp &nbsp Ajouter un membre au groupe (mode administrateur)
 				</div>
 				<div id=addMembre>
 					<form method="get">
 						<p>Nom du membre <input type="text" name="nom"  required /></p>
 						<p>Prenom du membre <input type="text" name="prenom" required /></p>
-						<p>Description <input type="text" name="description" required /></p>
-						<p><button type="submit" name="button1" value="ajout">Ajouter le membre</button></p>
+						<p>Description <textarea name="description" required>description </textarea></p>
+						<p>Mot de passe <input type="password" name="mdp" required /></p>
+						<p>Confirmation mot de passe <input type="password" name="mdp_conf" required />  <?php if($resAddMember == 2){echo("Les deux mot de passes ne correspondent pas");}?></p>
+						<p><button type="submit" name="btn_ajout_membre" value="ajout">Ajouter le membre</button> <?php if($resAddMember == 1){echo("Le membre a bien été ajouté");}elseif($resAddMember == 3){echo("Une erreur c'est produite...'");}?> </p>
 					</form>
 				</div>
 			</div>
