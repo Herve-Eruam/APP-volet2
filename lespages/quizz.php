9a74e162815ad23a4683689ed5ef3d478fad008a
zaide <zaide>
2022-05-25 15:35:38 +0200

    Quizz

--- lespages/quizz.php
+++ lespages/quizz.php
@@ -3,6 +3,42 @@ session_save_path("../sessionPhp");
 session_start();
 
 include 'php_functions/bdd_connect.php';
+
+
+	function getNote(){
+		$note=0;
+
+		if($_POST['question1']=='reponse1'){
+			$note=$note +1;
+		}
+
+		if($_POST['question2']=='reponse2' ){
+			$note=$note+1;	}
+
+		if($_POST['question3']=='reponse2' ){
+			$note=$note+1;
+		}
+
+		if($_POST['question4']=='reponse2'){
+			$note=$note+1;
+		}
+
+		if($_POST['question5']=='reponse3'){
+			$note=$note+1;
+		}
+
+		if($_POST['question6']=='reponse1'){
+			$note=$note+1;
+		}
+
+		if($_POST['question7']=='reponse4' ){
+			$note=$note+1;
+		}
+		
+		echo "Votre note pour ce test est : ".$note."/7";
+		
+	}
+
 ?>
 
 <html> 
@@ -28,77 +64,46 @@ include 'php_functions/bdd_connect.php';
 			</div>
 
             <div id=ShowQCM>
-					<form action="" method="get">
+					<form action="quizz.php" method="post">
 						<p name="question"> 
+						
 							<?php
 								$result = BDD_request("SELECT * FROM AppVOLET_Quizz");
 								$note=0;
-
 
 								while ($val = mysqli_fetch_assoc($result)) {
 									//echo "<option" .$selected ." value=".$val['id_membre'].">".$result."</option>";
 									//echo"<label><input type='radio' name='cheval' value='blanc' />"."blanc"."</label><br />";
 									$it=$val['id_question'];
-
-									echo "<p>"." "."$it)"."".$val['question']."</p>"."<br/>";
-									echo"<input type='radio' name='question1' value='reponse1' />".$val['reponse1']."</label><br />";
-									echo"<input type='radio' name='question1' value='reponse2' />".$val['reponse2']."</label><br />";
-									echo"<input type='radio' name='question1' value='reponse3' />".$val['reponse3']."</label><br />";
-									echo"<input type='radio' name='question1' value='reponse4'/>".$val['reponse4']."</label><br />";
 									
-
-									$note=$note + getAnswer($it);
+									echo "<li id='quest'>".$val['question']."</li><br>";
+									
+									echo "<select name='question" .$it ."'>"."<br>";
+									echo"<option type='radio'  value='reponse1' />".$val['reponse1']."</option><br />";
+									echo"<option type='radio'  value='reponse2' />".$val['reponse2']."</option><br />";
+									echo"<option type='radio'  value='reponse3' />".$val['reponse3']."</option><br />";
+									echo"<option type='radio' value='reponse4'/>".$val['reponse4']."</option><br />";
+									echo"</select>";
+									echo"<br><br>";
+	
 								}	 
+
 								
 							?>
 						
-					<input type="submit" name="" value="Valider">
-					<input type="submit" name="" value="Effacer">
-					<?php 
-					 //if(isset($GET_))
-					?>
+					<input type="submit" name="Valider" value="Valider">
 					</form>
+
+
+					<?php 
+					 if(isset($_POST['question4']) == true){
+						getNote();
+					 }
+					?>
+					
 				</div>
 			
-			<!-- GESTION DU QCM POUR RENVOYER LA NOTE -->
-			<?php 
-				function getAnswer($it){
-					$note=0;
-
-					if(isset($_GET['reponse1']) && $it==1){
-						//echo "Question". "$it".": La bonne reponse etait".""
-						$note=$note +(20/7);
-					}
-
-					if(isset($_GET['']) && $it==2){
-						$note=$note+(20/7);	}
-
-					if(isset($_GET['']) && $it==3){
-						$note=$note+(20/7);
-					}
-
-					if(isset($_GET['']) && $it==4){
-						$note=$note+(20/7);
-					}
-
-					if(isset($_GET['']) && $it==5){
-						$note=$note+(20/7);
-					}
-
-					if(isset($_GET['']) && $it==6){
-						$note=$note+(20/7);
-					}
-
-					if(isset($_GET['']) && $it==7){
-						$note=$note+(20/7);
-					}
-					
-
-					/*CORRECTION*/
-
-					return $note;
-				}
-			?> 	
+		
                             
 			
 		</div>
