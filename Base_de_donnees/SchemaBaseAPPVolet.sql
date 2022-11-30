eb6cb4d59283ef9ae3c6447c213435fc5a5bfc96
lvalet <lionel.valet@univ-smb.fr>
2022-11-30 17:49:50 +0100

    Ajustement des éléments de la base de données

--- Base_de_donnees/SchemaBaseAPPVolet.sql
+++ Base_de_donnees/SchemaBaseAPPVolet.sql
@@ -1,10 +1,12 @@
-drop table if exists AppVolet_Groupe cascade;
-drop table if exists AppVOLET_Techno cascade;
-drop table if exists AppVOLET_Mesures cascade;
-drop table if exists AppVOLET_Sugest cascade;
+ALTER TABLE `2024_VOLET_Mesures` DROP FOREIGN KEY fk_capteur; 
+
+drop table if exists `2024_VOLET_Groupe` cascade;
+drop table if exists `2024_VOLET_Techno` cascade;
+drop table if exists `2024_VOLET_Mesures` cascade;
+drop table if exists `2024_VOLET_Sugest` cascade;
 
 
-create table AppVolet_Groupe(id_membre integer auto_increment primary key,
+create table `2024_VOLET_Groupe` (id_membre integer auto_increment primary key,
 		nom varchar(30),
 		prenom varchar(30),
 		genre varchar(1), /* H ou F */
@@ -12,26 +14,22 @@ create table AppVolet_Groupe(id_membre integer auto_increment primary key,
 		nom_photo varchar(20)
 		);
 
-create table AppVOLET_Techno(id_techno integer auto_increment primary key,
-		nom varchar(10),
+create table `2024_VOLET_Techno` (id_techno integer auto_increment primary key,
+		nom varchar(30),
 		types varchar(30), /*capteur ou actionneur ou intelligence*/
 		position varchar(50), /* interieur ou exterieur */
-		unite varchar(5), /* degre,lux*/
-		descript varchar(300) /*Justification*/
+		unite varchar(20), /* degre,lux*/
+		descript varchar(800) /*Justification*/
 		);
 
-create table AppVOLET_Mesures(id_mesure integer auto_increment primary key,
+create table `2024_VOLET_Mesures` (id_mesure integer auto_increment primary key,
 		id_techno integer,
 		instant timestamp,
 		valeur numeric
 		);
 
-
-
-
-
-
-alter table AppVOLET_Mesures add constraint fk_capteur foreign key (id_techno) references AppVOLET_Techno(id_techno);
+ 
+alter table `2024_VOLET_Mesures` add constraint fk_capteur foreign key (id_techno) references `2024_VOLET_Techno`(id_techno);
 /*
 alter table candidat add constraint fk_membre_f foreign key (id_jury_f) references membre_jury(id_membre);
 alter table candidat add constraint fk_membre_m foreign key (id_jury_m) references membre_jury(id_membre);
